b9eac23cfd927f008f6330f5fc900ec853830bbf
fldh753 <fldh753@gmail.com>
2023-01-26 21:15:43 +0900

    0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
     public bool bScene = false;
     public bool cScene = false;
     [Header("AScene")]
-    public int currentBoxNum = -1;
+    public int currentBoxNum;
     [Header("시간관련")]
     public Time time;
     [Header("플레이어 스폰")]
