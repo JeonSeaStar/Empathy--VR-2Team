9dd098dd9f4ab1c36fd5b7317d4787fcbca15044
fldh753 <fldh753@gmail.com>
2023-02-08 19:26:26 +0900

    20230208

---
+++ Assets/TenSecond.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenSecond : MonoBehaviour
+{
+    public ButtonMethod buttonMethod;
+    int second = 10;
+    void Start()
+    {
+        StartCoroutine(TenSeconds());
+    }
+    IEnumerator TenSeconds()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        buttonMethod.GotoRoom();
+        StopCoroutine(TenSeconds());
+    }
+}
