6376586d526ce9ebb862cea95a5b63a486d5dcea
fldh753 <fldh753@gmail.com>
2023-02-10 21:38:19 +0900

    0

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -10,22 +10,33 @@ public class Menu : MonoBehaviour
  
     void Update()
     {
-        if(OVRInput.GetDown(OVRInput.RawButton.Start))
+        OnMenu();      
+    }
+
+    void OnMenu()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.Start)&& !isuiOn)
         {
-            isuiOn = true;
-            menu.SetActive(true);
-            Time.timeScale = 0;
-            //if (OVRInput.GetDown(OVRInput.RawButton.Start))
-            //{
-            //    isuiOn = false;
-            //    menu.SetActive(false);
-            //    Time.timeScale = 1;
-            //    Debug.Log("¾ÈµÅ?");
-            //}
+            if(SceneManager.GetActiveScene().buildIndex !=5 && 
+                SceneManager.GetActiveScene().buildIndex != 0 && 
+                SceneManager.GetActiveScene().buildIndex != 1)
+            {
+                isuiOn = true;
+                menu.SetActive(true);
+                Time.timeScale = 0;
+            }
         }
     }
+
     public void OffMenu()
     {
+        isuiOn = false;
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void GotoRoom()
+    {
+        LodingSceneManager.LoadScene("Room", "Non");
         isuiOn = false;
         menu.SetActive(false);
         Time.timeScale = 1;
