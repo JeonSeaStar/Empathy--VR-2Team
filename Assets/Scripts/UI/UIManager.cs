7b1990d2fc74bca0ae664cf839bf1573901265d5
fldh753 <fldh753@gmail.com>
2023-02-27 11:14:58 +0900

    0

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -53,12 +53,12 @@ public class UIManager : MonoBehaviour
 
         if (isClearPos)
         {
-            if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+            if (OVRInput.Get(OVRInput.RawButton.Any))
             {
                 isClearPos = false;
                 LodingSceneManager.LoadScene("Room", "Non");
             }
-            if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+            if (OVRInput.Get(OVRInput.RawButton.Any))
             {
                 isClearPos = false;
                 LodingSceneManager.LoadScene("Room", "Non");
