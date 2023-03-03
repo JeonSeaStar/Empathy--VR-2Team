19a5f7692ec135674a005fed8876c5a455f5a1b4
fldh753 <fldh753@gmail.com>
2023-03-03 19:20:54 +0900

    0

--- Assets/Scripts/A/InterectPolice.cs
+++ Assets/Scripts/A/InterectPolice.cs
@@ -9,41 +9,79 @@ public class InterectPolice : MonoBehaviour
 {
     public Animator ani;
     public GameObject chat;
+    Quaternion q;
     Quaternion first;
     public Transform player;
     bool islookplayer = false;
+    float t = 0;
     private void Start()
     {
         first = transform.rotation;
         chat.SetActive(false);
     }
 
-    private void Update()
-    {
-        if(islookplayer)
-        {
-            transform.LookAt(player);
-        }
-    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            t = 0;
+            q = Quaternion.LookRotation(CatRotator(player.transform.position));
+            //ani.SetTrigger("ThumbUp");
             islookplayer = true;
-            ani.SetTrigger("ThumbUp");          
-            StartCoroutine(OnChatbubble());
+            if (islookplayer)
+                ani.SetTrigger("ThumbUp");
+            StartCoroutine(InterectRotate());
         }
     }
 
-    IEnumerator OnChatbubble()
+    //IEnumerator OnChatbubble()
+    //{
+    //    chat.SetActive(true);
+    //    yield return new WaitForSecondsRealtime(3);
+    //    islookplayer = false;
+    //    chat.SetActive(false);
+    //    //transform.rotation = Quaternion.Lerp(transform.rotation, first, t);
+    //    StopCoroutine(OnChatbubble());
+    //}
+
+    Vector3 CatRotator(Vector3 destination)
     {
+        return destination - transform.position;
+    }
+
+    IEnumerator InterectRotate()
+    {
+        t += 0.01f;
+        yield return new WaitForSeconds(0.1f);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, q, t);
+        transform.LookAt(player.transform);
         chat.SetActive(true);
-        yield return new WaitForSecondsRealtime(3);
-        chat.SetActive(false);
-        transform.rotation = first;
-        islookplayer = false;
-        StopCoroutine(OnChatbubble());
+        if (t < 1)
+        {
+            StartCoroutine(InterectRotate());
+        }
+        else
+        {
+            //chat.SetActive(true);
+            yield return new WaitForSecondsRealtime(3);
+            chat.SetActive(false);
+            StartCoroutine(InterectRotate2());
+        }
+    }
 
-
+    IEnumerator InterectRotate2()
+    {
+        t -= 0.01f;
+        yield return new WaitForSeconds(0.01f);
+        transform.rotation = Quaternion.Lerp(first, transform.rotation, t);
+        if (t > 0)
+        {
+            StartCoroutine(InterectRotate2());
+        }
+        else
+        {
+            islookplayer = false;
+        }
     }
 }
